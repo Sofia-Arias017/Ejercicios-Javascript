065a14967bc235fb650acff63350e5ecc1207893
Sofia-Arias017 <189870116+Sofia-Arias017@users.noreply.github.com>
2025-03-21 02:38:40 -0500

    feat: :lipstick: Se hizzo el ejercicio de conversion de numeros a mes del año con switch

---
+++ Estructura-switch/conversion-de-numero-a-mes-del-año-con-switch/main.js
@@ -0,0 +1,48 @@
+function obtenerNombreDelMes() {
+
+    let numeroMes = parseInt(prompt("Ingresa un número del 1 al 12 para conocer el mes correspondiente:"))
+    
+        switch (numeroMes) {
+            case 1:
+                console.log("Enero")
+                break
+            case 2:
+                console.log("Febrero")
+                break
+            case 3:
+                console.log("Marzo")
+                break
+            case 4:
+                console.log("Abril")
+                break
+            case 5:
+                console.log("Mayo")
+                break
+            case 6:
+                console.log("Junio")
+                break
+            case 7:
+                console.log("Julio")
+                break
+            case 8:
+                console.log("Agosto")
+                break
+            case 9:
+                console.log("Septiembre")
+                break
+            case 10:
+                console.log("Octubre")
+                break
+            case 11:
+                console.log("Noviembre")
+                break
+            case 12:
+                console.log("Diciembre")
+                break
+            default:
+                console.log("Número fuera de rango. Debes ingresar un valor entre 1 y 12.")
+        }
+    }
+    
+    obtenerNombreDelMes()
+    
