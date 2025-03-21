5c5e8b7942029551384d6984bb3ac3719def6f04
Sofia-Arias017 <189870116+Sofia-Arias017@users.noreply.github.com>
2025-03-20 22:55:24 -0500

    feat: :lipstick: Se hizo el ejercicio de entrada para tu pelicula favorita

---
+++ Operadores-Logicos/Una-entrada-para-tu-pelicula-favorita/main.js
@@ -0,0 +1,10 @@
+let peliculaElegida = prompt(
+    "Elige tu película favorita de la siguiente lista:\n" +
+    "1. El Rey Leon\n" +
+    "2. Harry Potter\n" +
+    "3. Star Wars\n" +
+    "4. Spider Man\n" +
+    "5. Titanic"
+)
+
+console.log("Has elegido: " + peliculaElegida)
