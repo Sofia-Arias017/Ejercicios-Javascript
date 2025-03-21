d311e62a68b50e172d2a96bdf25b6260598ecbe2
Sofia-Arias017 <189870116+Sofia-Arias017@users.noreply.github.com>
2025-03-20 23:35:30 -0500

    feat: :lipstick: Se hizo el ejercicio de comparacion estricta de cadenas

---
+++ Operadores-de-Comparacion/Comparacion-Estricta-de-cadenas/main.js
@@ -0,0 +1,8 @@
+let cadena1 = prompt("Ingresa la primera cadena de texto:")
+let cadena2 = prompt("Ingresa la segunda cadena de texto:")
+
+if (cadena1 === cadena2) {
+    console.log("Las cadenas son idénticas en valor y tipo.")
+} else {
+    console.log("Las cadenas son diferentes.")
+}
